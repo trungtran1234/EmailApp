311d8b48f3d4d550ab31eb7b798201a5397cc78a
Trung Tran <trungtran63470@gmail.com>
2023-04-19 18:24:42 -0700

    registration and login works with database!

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,9 @@
+from app import db
 from glob import escape
-from flask import flash, redirect, render_template
+from flask import flash, redirect, render_template, request, session, url_for
+from flask_login import login_user
+
+from app.models import User
 from .forms import LoginForm, RegisterForm
 from app import myapp_obj
 
@@ -15,17 +19,25 @@ def hello():
 @myapp_obj.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+
     if form.validate_on_submit():
-        flash(f'Here are the input {form.username.data} and {form.password.data}')
-        return redirect('/')
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if user.check_password(form.password.data):
+                return redirect(url_for('index'))
+        return '<h1>Invalid username or password</h1>'
     return render_template('login.html', form=form)
 
 @myapp_obj.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
+
     if form.validate_on_submit():
-        flash(f'Here are the input {form.username.data} and {form.password.data} and {form.email.data}')
-        return redirect('/')
+        new_account = User(username=form.username.data, email = form.email.data)
+        new_account.set_password(form.password.data)
+        db.session.add(new_account)
+        db.session.commit()
+        return '<h1>Account created successfully</h1>'
     return render_template('register.html', form=form)
 
 @myapp_obj.route("/members/<string:name>/")
