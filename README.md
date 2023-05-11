a5a532672dbc4e58f1321ec2cf9f41afd94fe1ae
Jasper Nguyen <nguyenxjasper@icloud.com>
2023-05-10 21:55:34 -0700

    Searh bar is now implemented and working!!!

--- README.md
+++ README.md
@@ -1,5 +1,49 @@
-# Sigma Mail (Smail)
+# Sigma Mail (S-mail)
+## Contributors
 - Tri Nguyen - ngtri1809
 - Jasper Nguyen - jasper-nguyen
 - Kevin Aquino - Fireality
 - Trung Tran - trungtran1234
+
+## Introduction
+Sigma Mail is a modern, web-based email application that allows registered users to manage their emails online. With features like composing email and todo list, this app makes it easy to stay on top of your email game. Built with python, html, and css, Sigma mail is a reliable and efficient way to manage your emails from anywhere with an internet connection.
+
+## Installation  
+1. Clone the repository:   
+SSH Clone:  
+git clone git@github.com:ngtri1809/project131.git   
+HTTPS Clone:  
+https://github.com/ngtri1809/project131.git  
+
+2. Use pip to install libraries:   
+pip install sqlalchemy, flask, flask_login, flask_sqlalchemy, sqlalchemy, flask_wtf, wtf_forms, datetime, werkzeug
+
+3. Run the program (in the terminal):  
+cd project131  
+python3 run.py  
+Link to go on the webpage:  
+http://127.0.0.1:5000  
+
+4. Exit the program:  
+In the terminal, press ctrl + C and it will end the program  
+
+## How to use
+1. Create account: The user can create an account
+2. Log-in: User can log in using the account that they created
+3. Log-out: User can log out of their account
+4. Delete account: The user can delete their account through the settings page
+5. Changing password: The user can change their password through the settings page
+6. Compose: User can compose an email where they will fill in the recipient, subject, and body of the email. They can click "send" to send it to the selected user afterwards
+7. Sort-by-date: User can view their message sorted by date by selecting the order in the dropdown menu and clicking "sort"
+8. Create to-do list: The user can create a to-do list, update, and delete item inside the list
+
+## Technologies Used
+1. Coding Languages: Python, HTML, CSS
+2. Libraries: sqlalchemy, flask, flask_login, flask_wtf, wtforms, glob, flask_sqlalchemy, datetime, werkzeug
+3. Collaboration platforms: Github and Discord
+
+## Acknowledgments
+- Tri Nguyen - Implemented todo list
+- Jasper Nguyen - Implemented sort messages by date
+- Kevin Aquino - Implemented change password
+- Trung Tran - Implemented compose,login,logout,register,delete account
--- app/models.py
+++ app/models.py
@@ -1,5 +1,4 @@
 from datetime import datetime
-import pytz
 from flask_login import LoginManager, UserMixin
 from app import db, login_manager
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -12,6 +11,11 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(32), nullable=False)
     password = db.Column(db.String(32), nullable=False)
     email = db.Column(db.String(100), nullable=False)
+    bio = db.Column(db.String(100), nullable = False, default = '')
+    name = db.Column(db.String(32), nullable = False, default = '')
+    
+
+    friends = db.relationship('Friend', backref='user', lazy='dynamic')
 
     def set_password(self, password):
         self.password = generate_password_hash(password)
@@ -24,7 +28,17 @@ class User(db.Model, UserMixin):
     
     def received_messages(self):
         return Message.query.filter_by(recipient=self).order_by(Message.timestamp.desc()).all()
+class Friend(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    #Define relationship between user and friend
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    friend_of = db.relationship('User', foreign_keys=[user_id])
 
+    def __repr__(self):
+        return f"Friend('{self.name}', '{self.email}')"
+    
 #Message object
 class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -32,11 +46,19 @@ class Message(db.Model):
     recipient_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     subject = db.Column(db.String(100), nullable=False)
     body = db.Column(db.String(500), nullable=False)
-    timestamp = db.Column(db.DateTime, default=datetime.now(pytz.timezone('US/Pacific')))
+    timestamp = db.Column(db.DateTime, default=datetime.now())
 
     sender = db.relationship('User', foreign_keys=[sender_id])
     recipient = db.relationship('User', foreign_keys=[recipient_id])
-    
+
+class Todo(db.Model):
+    task_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False) # set the foreign key from user
+    name = db.Column(db.String(100)) # name of the task
+    done = db.Column(db.Boolean) #user mark as done or undone
+    #Define relationship between user and todo list's tasks using the user_id 
+    user = db.relationship('User', foreign_keys=[user_id])
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
